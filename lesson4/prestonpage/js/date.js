277a704048c1954d92c1de153d1f28d5752dec87
Elise <elisechristensen11@gmail.com>
2021-05-14 11:59:03 -0700

    date

--- lesson4/prestonpage/js/date.js
+++ lesson4/prestonpage/js/date.js
@@ -10,4 +10,4 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 // long, medium, short options ... try them
 
-datefield.innerHTML = `<em>${fulldate}</em>`;
+datefield.innerHTML = `${fulldate}`;
