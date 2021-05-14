23f767e64546a169ade8f82f730482354205925c
Elise <elisechristensen11@gmail.com>
2021-05-14 08:49:46 -0700

    date adjust

--- lesson4/prestonpage/js/date.js
+++ lesson4/prestonpage/js/date.js
@@ -1,2 +1,13 @@
-document.querySelector('#lastmod').innerHTML = document.lastModified;
-document.querySelector('#year').innerHTML =  new Date().getFullYear();
+//document.querySelector('#lastmod').innerHTML = document.lastModified;
+//document.querySelector('#year').innerHTML =  new Date().getFullYear();
+
+const datefield = document.querySelector("date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
