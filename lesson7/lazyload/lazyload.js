39ee0a58971ab824348db7e0c3afb93972387b89
Elise <elisechristensen11@gmail.com>
2021-05-31 15:53:20 -0700

    lazy loading

---
+++ lesson7/lazyload/lazyload.js
@@ -0,0 +1,76 @@
+
+/* Another Sample of how to do it, although I can't get this one to work, it gets stuck on the placeholder:
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  } */
+
+
+
+
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  }); 
+
+
+
+
+/*
+This is the code from the video:
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = image.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+
+    entries.forEach(entry => {
+        if (!entries.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
+*/
+
+
---
+++ lesson7/prestonpage/js/index.js
@@ -0,0 +1,72 @@
+
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+//DATES
+
+document.querySelector('#lastmod').innerHTML = document.lastModified;
+document.querySelector('#year').innerHTML =  new Date().getFullYear();
+
+const datefield = document.querySelector("#currentyear");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `${fulldate}`;
+
+
+let thedate = new Date();
+	if (thedate.getDay() == 5) {
+		document.querySelector('.banner').style.display="block";
+	}
+
+//WINDCHILL
+
+
+/* Input: Temperature and Windspeed
+ * Processing: create two functions - one for the formula and one that calls to the formula and displays the answer
+ Divide formula into three parts to make calculation less muddled (for me)
+ return the result
+ doInputOutput function receives the value from the windchill function
+ * Output: Calculated Wind Chill
+ */
+
+ var tempF = document.querySelector('#temp').innerHTML;
+ var speed = document.querySelector('#speed').innerHTML;
+ console.log(tempF, speed);
+
+
+
+
+    if (tempF <= 50 && speed > 3) {
+        let answer = windChill(tempF, speed);
+        document.querySelector('#windOutput').innerHTML = answer + '&#176;F';
+
+    }
+       
+    else {  document.querySelector('#windOutput').innerHTML = 'N/A';}
+      
+
+    document.querySelector('#windOutput').innerHTML = answer;
+
+
+
+ function windChill(tempF, speed) {
+     let partOne = 35.74 + (0.6215 * tempF);
+     let firstExp = 35.74 * (Math.pow(speed, 0.16));
+     let secondExp = 0.4275 * tempF * (Math.pow(speed, 0.16));
+     let result = partOne - firstExp + secondExp;
+     let result2 = Math.trunc(result);
+     return result2;
+    }
