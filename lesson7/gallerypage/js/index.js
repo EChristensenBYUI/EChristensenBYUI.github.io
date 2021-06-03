f320887974806de1e6f3ce5a51042667d3fbe9ee
Elise <elisechristensen11@gmail.com>
2021-06-02 21:17:04 -0700

    gallery1

---
+++ lesson7/gallerypage/js/index.js
@@ -0,0 +1,48 @@
+
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+//DATES
+
+document.querySelector('#lastmod').innerHTML = document.lastModified;
+document.querySelector('#year').innerHTML =  new Date().getFullYear();
+
+const datefield = document.querySelector("#currentyear");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `${fulldate}`;
+
+
+let thedate = new Date();
+	if (thedate.getDay() == 5) {
+		document.querySelector('.banner').style.display="block";
+	}
+
+
+
+    //Lazy Load
+
+    let imagesToLoad = document.querySelectorAll('img[data-src]');
+    const loadImages = (image) => {
+      image.setAttribute('src', image.getAttribute('data-src'));
+      image.onload = () => {
+        image.removeAttribute('data-src');
+      };
+    };
+    
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+      }); 
