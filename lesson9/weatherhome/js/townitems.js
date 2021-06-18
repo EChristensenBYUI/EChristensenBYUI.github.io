64150c6c82df96e31ecf8f3fc7a928b915bd8891
Elise <elisechristensen11@gmail.com>
2021-06-18 12:10:27 -0700

    another attempt

--- lesson9/weatherhome/js/townitems.js
+++ lesson9/weatherhome/js/townitems.js
@@ -14,10 +14,13 @@ fetch(requestURL)
 
     const towns = jsonObject['towns'];
 
+
     //const town = towns.filter(x => x.name == 'Preston' || x.name == 'Soda Springs' || x.name == 'Fish Haven');
 
     for (let i = 0; i < towns.length; i++ ) {
-        if (towns[i].name == 'Preston' || towns[i].name == 'Soda Springs' || towns[i].name == 'Fish Haven') {
+
+        console.log(towns[i].name);
+        //if (towns[i].name == 'Preston' || towns[i].name == 'Soda Springs' || towns[i].name == 'Fish Haven') {
             let card = document.createElement('div');
             let h2 = document.createElement('h2');
             let h3 = document.createElement('h3');
@@ -25,7 +28,7 @@ fetch(requestURL)
             let currentPopulation = document.createElement('p');
             let averageRainfall = document.createElement('p');
 
-            h2.textContent = towns[i].name;
+            h2.innerHTML = towns[i].name;
             h3.textContent = towns[i].motto;
 
             yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
@@ -38,11 +41,19 @@ fetch(requestURL)
             card.appendChild(currentPopulation);
             card.appendChild(averageRainfall);
 
-          if (towns[i].name == 'Soda Springs') {
+          if (towns[i].name == "Soda Springs") {
                 document.querySelector('#soda').appendChild(card);
            } 
 
-        }
+           if (towns[i].name == "Fish Haven") {
+            document.querySelector('#fish').appendChild(card);
+       } 
+
+       if (towns[i].name == "Preston") {
+        document.querySelector('#preston').appendChild(card);
+   } 
+
+       //}
 
 
 
