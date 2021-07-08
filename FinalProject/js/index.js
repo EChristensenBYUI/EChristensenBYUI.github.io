03c47437fad0f3ce3cc301d241d3d6625865387c
Elise <elisechristensen11@gmail.com>
2021-07-08 13:18:50 -0700

    home page 2

--- FinalProject/js/index.js
+++ FinalProject/js/index.js
@@ -7,3 +7,16 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+//Last modified date
+
+document.querySelector('#lastmod').innerHTML = document.lastModified;
+document.querySelector('#year').innerHTML =  new Date().getFullYear();
+
+const datefield = document.querySelector("#currentyear");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
